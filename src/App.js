7d5f2921f27c5eac59e8a204c2b9745376e98b15
Colton <coltonfalkner@gmail.com>
2022-02-17 21:37:17 -0500

    initial structure for UI component layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.scss'
+import ComparisonCard from './Components/Comparison/ComparisonCard'
+import MovieCard from './Components/MovieCard/MovieCard'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div>
+      <ComparisonCard />
+      <MovieCard />
+      <MovieCard />
+    </div>
+  )
 }
 
 export default App
