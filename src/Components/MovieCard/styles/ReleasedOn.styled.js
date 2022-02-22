8c86aad247ffc94e610e2185ad23109b469d9e68
Colton <coltonfalkner@gmail.com>
2022-02-22 14:56:21 -0500

    abstracted styled-components to separate directory

---
+++ src/Components/MovieCard/styles/ReleasedOn.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const ReleasedOn = styled.div`
+  display: inline-block;
+  font-size: 3rem;
+  font-weight: 200;
+`
+
+export default ReleasedOn
---
+++ src/Components/MovieCard/styles/TicketDate.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const TicketDate = styled.p`
+  display: inline-flex;
+  font-size: 3.5rem;
+  padding: 1rem 0 2rem 4rem;
+  color: #5f5c58;
+  font-weight: 600;
+`
+
+export default TicketDate
