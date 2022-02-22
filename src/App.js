ca16e6e17fb2ca0799717b57690898e84c5051a6
Colton <coltonfalkner@gmail.com>
2022-02-22 14:57:35 -0500

    added clock SVG and added applied global styled component to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.scss'
 import ComparisonCard from './Components/Comparison/ComparisonCard'
 import MovieCard from './Components/MovieCard/MovieCard'
+import GlobalStyles from './Components/Global'
+import { Global } from 'recharts'
 
 function App() {
   return (
-    <div>
-      <ComparisonCard />
-      <MovieCard title='Movie 1' releaseDate='May 3, 2015' />
-      {/* <MovieCard /> */}
-    </div>
+    <>
+      <GlobalStyles />
+      {/* <ComparisonCard /> */}
+      <MovieCard
+        title='Movie 1'
+        releaseDate='04 Jan 2015'
+        ticketDate='May 3, 2015'
+      />
+      {/* <MovieCard
+        title='Movie 2'
+        releaseDate='6 Feb 2015'
+        ticketDate='May 3, 2015'
+      /> */}
+    </>
   )
 }
 
