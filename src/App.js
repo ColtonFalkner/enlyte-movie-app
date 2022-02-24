72672e9d8daaea83a256ee424c8e9634fc1e8b74
Colton <coltonfalkner@gmail.com>
2022-02-24 16:20:29 -0500

    created Wrapper element for UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import ComparisonCard from './Components/Comparison/ComparisonCard'
 import MovieCard from './Components/MovieCard/MovieCard'
 import GlobalStyles from './Components/Global'
-
-// import { Global } from 'recharts'
+import UIWrapper from './Components/UIWrapper'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <ComparisonCard />
-      <MovieCard
-        title='Movie 1'
-        releaseDate='04 Jan 2015'
-        ticketDate='May 3, 2015'
-      />
-      {/* <MovieCard
-        title='Movie 2'
-        releaseDate='6 Feb 2015'
-        ticketDate='May 3, 2015'
-      /> */}
+      <UIWrapper>
+        <ComparisonCard />
+
+        <MovieCard
+          title='Movie 1'
+          releaseDate='04 Jan 2015'
+          ticketDate='May 3, 2015'
+        />
+        <MovieCard
+          title='Movie 2'
+          releaseDate='6 Feb 2015'
+          ticketDate='May 3, 2015'
+        />
+      </UIWrapper>
     </>
   )
 }
---
+++ src/Components/UIWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const UIWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10rem auto;
+  width: 200rem;
+`
+
+export default UIWrapper
