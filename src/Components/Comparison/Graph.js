0f64703f7d1ae6d81e4c26c47f3832b76fca1b53
Colton <coltonfalkner@gmail.com>
2022-02-23 22:37:18 -0500

    Graph component complete

--- src/Components/Comparison/Graph.js
+++ src/Components/Comparison/Graph.js
@@ -1,8 +1,61 @@
 import React from 'react'
-import { LineChart, Line } from 'recharts'
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+  ReferenceDot,
+} from 'recharts'
 
-const Graph = () => {
-  return <div></div>
+const data = [
+  { name: 'Jan', Movie1: 62, Movie2: 48 },
+  { name: 'Feb', Movie1: 60, Movie2: 49 },
+  { name: 'Mar', Movie1: 40, Movie2: 60 },
+  { name: 'Apr', Movie1: 51, Movie2: 38 },
+  { name: 'May', Movie1: 36, Movie2: 55 },
+  { name: 'Jun', Movie1: 24, Movie2: 37 },
+  { name: 'Jul', Movie1: 39, Movie2: 30 },
+]
+
+const Graph = (props) => {
+  return (
+    <div style={{ width: 700, height: 300 }}>
+      <ResponsiveContainer>
+        <AreaChart
+          width={'auto'}
+          height={'auto'}
+          data={data}
+          margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid opacity={0.3} />
+          <XAxis dataKey='name' fontSize={15} />
+          <YAxis fontSize={15} interval={0} tickSize={5} />
+          <ReferenceDot alwaysShow={'true'} />
+          <Tooltip viewBox={'x: 0, y: 0, width: 20px, height: 15px'} />
+
+          <Area
+            type='monotone'
+            dataKey='Movie1'
+            stackId='1'
+            dot={{ stroke: 'RGB(159, 157, 155)', strokeWidth: 4, r: 2 }}
+            stroke='RGB(159, 157, 155)'
+            fill='RGBA(95, 92, 88, 0.2)'
+          />
+          <Area
+            type='monotone'
+            dataKey='Movie2'
+            stackId='2'
+            dot={{ stroke: 'RGB(79, 168, 6)', strokeWidth: 4, r: 2 }}
+            stroke='RGB(79, 168, 6)'
+            fill='RGBA(79, 168, 6, 0.2)'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
 export default Graph
